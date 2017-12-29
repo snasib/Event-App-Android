2641065539daff046a088cfc0b460bc3ac4a5292
Sadi Nasib <sadinasib@hotmail.com>
2017-12-29 13:30:29 +0100

    Login Page

--- app/src/main/java/com/android/redcarpet/AuthVerifierActivity.java
+++ app/src/main/java/com/android/redcarpet/AuthVerifierActivity.java
@@ -1,13 +1,31 @@
 package com.android.redcarpet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AuthVerifierActivity extends AppCompatActivity {
     private static final String TAG = AuthVerifierActivity.class.getSimpleName();
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (mAuth.getCurrentUser() == null) {
+            startActivity(new Intent(AuthVerifierActivity.this, LoginActivity.class));
+        } else {
+            startActivity(new Intent(AuthVerifierActivity.this, MainActivity.class));
+        }
     }
 }
